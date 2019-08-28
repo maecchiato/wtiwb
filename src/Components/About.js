5bb255c8c11e991f5d0842b4fc25bd8fefd196e0
Unknown <mayeghen@gmail.com>
2019-08-28 13:42:32 +0800

    edit --about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -26,17 +26,19 @@ class About extends React.Component {
       <div id="about-section" class="row">
         <p className="headline">Mula Noon, Hanggang Ngayon, Laking Waffle Time!</p>
         <img src={rec}></img>
-        <div id="about-body" className="container-fluid row">
-          <div className="col-xl-8 col-lg-8 col-md-9 col-sm-12 col-xs-12">
+        <div id="row2" className="container-fluid row">
+          <div id="about-body" className="col-xl-8 col-lg-8 col-md-9 col-sm-12 col-xs-12">
             <p className="subheadline">OUR HISTORY</p>
             <p className="body-reg">Waffle Time started on September of 1998 in the province of Iloilo, Philippines as a simple business venture. The first outlet was in The Atrium Mall, Gen. Luna St., Iloilo City. With 7 profitable outlets, the owners were very optimist that the concept and brand is ready for expansion.</p>
             <p className="body-reg">Today, Waffle Time boasts of over 400 outlets nationwide and is still actively expanding into new regions and territories. Waffle Time is aiming, from being Ang Pambansang Waffle ng Pilipinas, to become the Best Waffles in the World!</p>
+            <button className="btn-small">LEARN MORE</button>
           </div>
-          <div className="col-xl-4 col-lg-4 col-md-3 col-sm-12 col-xs-12 text-center">
+          <div className="container-fluid col-xl-4 col-lg-4 col-md-3 col-sm-12 col-xs-12 text-left">
             <img className="img-fluid mt-5" src={waffy} />
           </div>
-
+          
         </div>
+        
       </div>
 
     )
