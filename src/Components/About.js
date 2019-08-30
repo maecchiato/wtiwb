9066823a9bd962b2d0daddb5721669171c8efdd6
Mark Philip Gico <klaynne14@yahoo.com>
2019-08-30 08:20:05 +0800

    tentative mission vision update

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,9 @@
 import React from 'react';
-
+import { Route, Link } from 'react-router-dom';
+// import MissionVission from './Components/MissionVission';
 import waffy from './Layouts/img/waffy-mascot.png';
 import rec from './Layouts/img/Heading/rec.jpg'
+import MissionVision from './MissionVission';
 
 class About extends React.Component {
   render() {
@@ -14,7 +16,8 @@ class About extends React.Component {
             <p className="subheadline">OUR HISTORY</p>
             <p className="body-reg">Waffle Time started on September of 1998 in the province of Iloilo, Philippines as a simple business venture. The first outlet was in The Atrium Mall, Gen. Luna St., Iloilo City. With 7 profitable outlets, the owners were very optimist that the concept and brand is ready for expansion.</p>
             <p className="body-reg">Today, Waffle Time boasts of over 400 outlets nationwide and is still actively expanding into new regions and territories. Waffle Time is aiming, from being Ang Pambansang Waffle ng Pilipinas, to become the Best Waffles in the World!</p>
-            <button className="btn-small">LEARN MORE</button>
+            <button className="btn-small" onClick={this.try}>LEARN MORE</button>
+            <Route path="/Component/MissionVission" Component={MissionVision}/>
           </div>
           <div className="container-fluid col-xl-4 col-lg-4 col-md-3 col-sm-12 col-xs-12 text-left">
             <img className="img-fluid mt-12" src={waffy} />
