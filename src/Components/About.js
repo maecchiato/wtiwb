e999656784880a497f28643f8c2b16698d941ee2
Mark Philip Gico <klaynne14@yahoo.com>
2019-09-05 14:36:56 +0800

    fixed route error

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-// import MissionVission from './Components/MissionVission';
+
+import '../App.css';
+import {Link} from 'react-router-dom';
+// import { withRouter } from 'react-router-dom';
 import waffy from './Layouts/img/waffy-mascot.png';
-import rec from './Layouts/img/Heading/rec.jpg'
-import MissionVision from './MissionVission';
+import rec from './Layouts/img/Heading/rec.jpg';
+
+
 
 class About extends React.Component {
+  //  nextPath(path){
+  //    this.props.history.push(path);
+  //  }
   render() {
     return (
       <div id="about-section" class="row justify-content">
@@ -16,8 +22,10 @@ class About extends React.Component {
             <p className="subheadline">OUR HISTORY</p>
             <p className="body-reg">Waffle Time started on September of 1998 in the province of Iloilo, Philippines as a simple business venture. The first outlet was in The Atrium Mall, Gen. Luna St., Iloilo City. With 7 profitable outlets, the owners were very optimist that the concept and brand is ready for expansion.</p>
             <p className="body-reg">Today, Waffle Time boasts of over 400 outlets nationwide and is still actively expanding into new regions and territories. Waffle Time is aiming, from being Ang Pambansang Waffle ng Pilipinas, to become the Best Waffles in the World!</p>
-            <button className="btn-small" onClick={this.try}>LEARN MORE</button>
-            <Route path="/Component/MissionVission" Component={MissionVision}/>
+            <button className="btn-small" >LEARN MORE</button> 
+              {/* <Link to ="/MissionVission" className = "nav-link">LEARN MORE</Link> */}
+              
+              {/* <Link to="#mission-vision-section">LEARN MORE</Link> */}
           </div>
           <div className="container-fluid col-xl-4 col-lg-4 col-md-3 col-sm-12 col-xs-12 text-left">
             <img className="img-fluid mt-12" src={waffy} />
@@ -29,4 +37,5 @@ class About extends React.Component {
   }
 }
 
-export default About;
+
+export default About
