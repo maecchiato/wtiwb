d26db2dc2f232c4e0793b993fe11aa074fdd5e14
Unknown <mayeghen@gmail.com>
2019-09-10 08:33:06 +0800

    minor fix

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -28,69 +28,29 @@ class Navigation extends React.Component {
   render() {
     return (
 
-      // <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav id="navbar-header" className="navbar navbar-expand-lg navbar-light fixed-top">
 
-      //   {/* <!--  Show this only on mobile to medium screens  --> */}
-      //   <a class="navbar-brand d-lg-none" href="#">Navbar</a>
-
-      //   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
-      //     <span class="navbar-toggler-icon"></span>
-      //   </button>
-
-      //   {/* <!--  Use flexbox utility classes to change how the child elements are justified  --> */}
-      //   <div class="collapse navbar-collapse justify-content-between" id="navbarToggle">
-
-      //     <ul class="navbar-nav">
-      //       <li class="nav-item">
-      //         <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      //       </li>
-      //       <li class="nav-item">
-      //         <a class="nav-link" href="#">Link</a>
-      //       </li>
-      //       <li class="nav-item">
-      //         <a class="nav-link" href="#">Link</a>
-      //       </li>
-      //     </ul>
-      //     {/* <!--   Show this only lg screens and up   --> */}
-      //     <a class="navbar-brand d-none d-lg-block" href="#">Navbar</a>
-      //     <ul class="navbar-nav">
-      //       <li class="nav-item">
-      //         <a class="nav-link" href="#">Link</a>
-      //       </li>
-      //       <li class="nav-item">
-      //         <a class="nav-link" href="#">Link</a>
-      //       </li>
-      //       <li class="nav-item">
-      //         <a class="nav-link" href="#">Link</a>
-      //       </li>
-      //     </ul>
-      //   </div>
-      // </nav>
-
-      
-      <nav id = "navbar-header" className = "navbar navbar-expand-lg navbar-light fixed-top">
-        
-        <a className = "navbar-brand" href = "#">
-          <img id = "logo" src = {logo} alt = "" className = {this.state.hasClickedMENU ? 'd-none': null} />
+        <a className="navbar-brand" href="#">
+          <img id="logo" src={logo} alt="" className={this.state.hasClickedMENU ? 'd-none' : null} />
         </a>
-         <button ref={input => this.mobileMenuToggler = input} className = "navbar-toggler" onClick = {this.toggleClassMenu} type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className = "navbar-toggler-icon"></span>
+        <button ref={input => this.mobileMenuToggler = input} className="navbar-toggler" onClick={this.toggleClassMenu} type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-          <div className = "collapse navbar-collapse mt-lg-0 md-lg-0 mt-5" id="navbarNavAltMarkup">
-            <div className = "navbar-nav mr-auto">
-              <a className = "nav-item nav-link mr-md-1 text-center" onClick = {this.closeMenuToggler} href = "#about-section">ABOUT US</a>
-              <a className = "nav-item nav-link mr-md-1 text-center" onClick = {this.closeMenuToggler} href = "#waffles-section">OUR WAFFLES</a>
-              <a className = "nav-item nav-link mr-md-1 text-center" onClick = {this.closeMenuToggler} href = "#foundation-section">WTI FOUNDATION</a>
-              <a className = "nav-item nav-link mr-md-1 text-center" onClick = {this.closeMenuToggler} href = "#events-section">EVENTS</a>
-              </div>
-              <div className = "navbar-nav ml-auto">
-              <a className = "nav-item nav-link mr-md-1 text-center" onClick = {this.closeMenuToggler} href = "#party-package-section">PARTY PACKAGE</a>
-              <a className = "nav-item nav-link mr-md-1 text-center" onClick = {this.closeMenuToggler} href = "#map-section">STORES</a>
-              <a className = "nav-item nav-link mr-md-1 text-center" onClick = {this.closeMenuToggler} href = "#franchise-section">FRANCHISE</a>
-              <a className = "nav-item nav-link text-center"  onClick = {this.closeMenuToggler} href = "#v-pills-contact-tab">CONTACT US</a>
-              <p className = {!this.state.hasClickedMENU ? 'd-none': 'h6 text-center mobile-dropdown-copyright'}>Waffle Time Inc., © All Rights Reserved 2018</p>
-              
-            </div>
+        <div className="collapse navbar-collapse mt-lg-0 md-lg-0 mt-5" id="navbarNavAltMarkup">
+          <div className="navbar-nav mr-auto">
+            <a className="nav-item nav-link mr-md-1 text-center" onClick={this.closeMenuToggler} href="#about-section">ABOUT US</a>
+            <a className="nav-item nav-link mr-md-1 text-center" onClick={this.closeMenuToggler} href="#waffles-section">OUR WAFFLES</a>
+            <a className="nav-item nav-link mr-md-1 text-center" onClick={this.closeMenuToggler} href="#foundation-section">WTI FOUNDATION</a>
+            <a className="nav-item nav-link mr-md-1 text-center" onClick={this.closeMenuToggler} href="#events-section">EVENTS</a>
+          </div>
+          <div className="navbar-nav ml-auto">
+            <a className="nav-item nav-link mr-md-1 text-center" onClick={this.closeMenuToggler} href="#party-package-section">PARTY PACKAGE</a>
+            <a className="nav-item nav-link mr-md-1 text-center" onClick={this.closeMenuToggler} href="#map-section">STORES</a>
+            <a className="nav-item nav-link mr-md-1 text-center" onClick={this.closeMenuToggler} href="#franchise-section">FRANCHISE</a>
+            <a className="nav-item nav-link text-center" onClick={this.closeMenuToggler} href="#v-pills-contact-tab">CONTACT US</a>
+            <p className={!this.state.hasClickedMENU ? 'd-none' : 'h6 text-center mobile-dropdown-copyright'}>Waffle Time Inc., © All Rights Reserved 2018</p>
+
+          </div>
         </div>
       </nav>
     )
