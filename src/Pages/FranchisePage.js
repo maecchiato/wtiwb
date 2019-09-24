690902a5337924d49e5a778d653d393bf0b29388
Unknown <mayeghen@gmail.com>
2019-09-24 09:05:57 +0800

    est Events and Franchise Page + fillings section

---
+++ src/Pages/FranchisePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../Styles/FranchisePage.css';
+
+import Foundation from '../Components/Foundation.js';
+
+
+
+class FoundationPage extends React.Component {
+  render() {
+    return (
+      <div class="container-fluid">
+
+      </div>
+    )
+  }
+}
+export default FoundationPage;
