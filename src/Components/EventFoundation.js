37085c5c7f1963956f6807d7b8c44e9d20728c01
Unknown <mayeghen@gmail.com>
2019-10-02 08:40:44 +0800

    nav run build successful

--- src/Components/EventFoundation.js
+++ src/Components/EventFoundation.js
@@ -162,7 +162,7 @@ class EventFoundation extends React.Component{
          </Slider>
         </div>
         
-        <div className = "col-xl-2 col-lg-2 col-md-2 col-xs-12 col-sm-12 text-center mb-1">
+        <div className = "col-xl-1 col-lg-1 col-md-1 col-xs-auto col-sm-auto text-center mb-1">
         </div>
       </div>
 
